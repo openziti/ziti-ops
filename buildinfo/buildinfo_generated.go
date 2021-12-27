cfcdfd8e71a0505f2ef50a27415675c9223ecab2
ziti-ci <ziti-ci@netfoundry.io>
2021-12-27 22:03:43 +0000

    Release v0.1.4

--- buildinfo/buildinfo_generated.go
+++ buildinfo/buildinfo_generated.go
@@ -20,9 +20,9 @@
 package buildinfo
 
 const (
-	Version   = "v0.0.0"
-	Revision  = "5206c6cf6a7b"
+	Version   = "v0.1.4"
+	Revision  = "84e2f95ccb9a"
 	Branch    = "main"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2021-12-10T17:26:24-05:00"
+	BuildUser = ""
+	BuildDate = "2021-12-27T22:03:43Z"
 )
